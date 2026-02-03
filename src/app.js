c05d398627d75a9ba0b1fe75241072f8bae5f1fc
shankargoud99 <shankargoudp195@gmail.com>
2026-02-03 17:38:25 +0530

    exploring the get,put,post methods

--- src/app.js
+++ src/app.js
@@ -2,16 +2,32 @@ const express=require("express");
 
 const app=express();
 
-app.use("/",(req,res)=>{
-    res.send("Hello from shankar")
-});
+// app.use("/hello/2",(req,res)=>{
+//     res.send("Hello from hello2")
+// });
 
-app.use("/test",(req,res)=>{
-    res.send("Hello from test")
+// app.use("/test",(req,res)=>{
+//     res.send("Hello from test")
+// });
+// app.use("/hello",(req,res)=>{
+//     res.send("Hello from hello")
+// });
+// app.use("/",(req,res)=>{
+//     res.send("Hello from shankar")
+// });
+app.use("/user",(req,res)=>{
+    res.send("data shankar")
+})
+
+app.get("/user",(req,res)=>{
+    res.send({firstname:"shankar",lastname:"patil"})
+})
+app.post("/user",(req,res)=>{
+    res.send("Data saved successfully")
 });
-app.use("/hello",(req,res)=>{
-    res.send("Hello from hello")
-});
+app.delete("/user",(req,res)=>{
+    res.send("Deleted successfully")
+})
 
 app.listen(3000,()=>{
     console.log("server is connected to 3000")
