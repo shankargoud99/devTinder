84e4ad55f29eda4e69c47c2b71885c350ac22d76
shankargoud99 <shankargoudp195@gmail.com>
2026-02-05 00:57:27 +0530

    multiple route-handler

--- src/app.js
+++ src/app.js
@@ -2,43 +2,28 @@ const express=require("express");
 
 const app=express();
 
-// app.use("/hello/2",(req,res)=>{
-//     res.send("Hello from hello2")
-// });
 
-// app.use("/test",(req,res)=>{
-//     res.send("Hello from test")
-// });
-// app.use("/hello",(req,res)=>{
-//     res.send("Hello from hello")
-// });
-// app.use("/",(req,res)=>{
-//     res.send("Hello from shankar")
-// });
 
-app.get("/user/:userId", (req, res) => {
-    console.log(req.params);   // route param
-    console.log(req.query);    // query string
 
-    res.send({ firstname: "shankar", lastname: "patil" });
-});
+app.use("/user",[(req,res,next)=>{
+    console.log("hello from one");
+    // res.send("respponse 1");
+    next();
+},(req,res,next)=>{
+    console.log("helo from 2");
+    // res.send("response 2");
+    next();
+},(req,res,next)=>{
+    console.log("helo from 3");
+    // res.send("response 3");
+    next();
+},(req,res)=>{
+    console.log("helo from 4");
+    res.send("response 4");
+    // next();
+}]
+);
 
-app.get("/user/:userId", (req, res) => {
-    console.log(req.params)
-    res.send({ firstname: "shankar", lastname: "patil" });
-});
-// app.use("/user",(req,res)=>{
-//     res.send("data shankar")
-// })
-// app.get(/.*fly$/, (req, res) => {
-//     res.send({ firstname: "shankar", lastname: "patil" });
-// });
-// app.post("/user",(req,res)=>{
-//     res.send("Data saved successfully")
-// });
-// app.delete("/user",(req,res)=>{
-//     res.send("Deleted successfully")
-// })
 
 app.listen(3000,()=>{
     console.log("server is connected to 3000")
