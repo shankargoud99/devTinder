8b4868c4b6e3dec39103a46197256d83398a7d52
shankargoud99 <shankargoudp195@gmail.com>
2026-02-21 00:46:04 +0530

    get api

--- src/app.js
+++ src/app.js
@@ -5,17 +5,47 @@ const User=require("./models/user");
 
 app.use(express.json());
 
-app.post("/signup",async (req,res)=>{
+// app.post("/signup",async (req,res)=>{
     
-    //creating a new instance of the user model
-    const user=new User(req.body);
+//     //creating a new instance of the user model
+//     const user=new User(req.body);
+//     try{
+//     await user.save();
+//     res.send("User Added successfully");
+//     }catch(err){
+//         res.status(400).send("Error saving the user"+err.message)
+//     }
+//  });
+
+app.get("/user",async (req,res)=>{
+    const userEmail=req.body.emailId;
     try{
-    await user.save();
-    res.send("User Added successfully");
-    }catch(err){
-        res.status(400).send("Error saving the user"+err.message)
+        const users=await User.find({emailId:userEmail});
+       
+        if (users.length===0){
+            res.status(404).send("user not found");
+        }else{
+            res.send(users);
+        }
+        }catch{
+            res.status(400).send("something went wrong")
+        }
     }
- });
+    );
+app.get("/feed",async (req,res)=>{
+     try{
+        const users=await User.find();
+       
+        if (users.length===0){
+            res.status(404).send("user not found");
+        }else{
+            res.send(users);
+        }
+        }catch{
+            res.status(400).send("something went wrong")
+        }
+    }
+    );
 
 
 
